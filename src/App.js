d240b706db99cb4adeb3883bbd87a609a34020f0
suleman-suraj <69476432+suleman-suraj@users.noreply.github.com>
2022-04-22 15:45:53 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import './App.css';
+
+// import Header from "./components/Header";
+import Home from'./components/Home';
+// import Footer from "./components/Footer";
+
+// import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="">
+    {/* //   <Header />
+    //   <br />
+    //   <br />
+    //   <br />
+    //   <Footer /> */}
+    <Home/>
+    // </div>
+  );
+}
+
+export default App;
